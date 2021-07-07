3144009ad3637d5e319f3e42276759c13e991911
Penguinbot82 <kear.clint@gmail.com>
2021-07-07 21:40:19 +0000

    Learning the basics of deploying with Ansible

---
+++ grannych.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# For every [par] you get on the [course] I'm going to give you [money].
+
+course= ["Hole 1","Hole 2","Hole 3","Hole 4"]
+for par in course:
+    print(f" Nice par! {par}Here's a dollar")
